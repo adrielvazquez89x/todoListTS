4e748db5d91daf8c6a6baa1858807ac0e4e20bb9
Adriel Vazquez <adrialvaz@hotmail.com>
2023-11-09 17:23:24 -0300

    final

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,16 @@
+
+type TareaProps = {
+    tarea: string
+    borrarTarea: () => void
+}
+
+export const Tarea = ({ tarea, borrarTarea }: TareaProps) => {
+    return (
+        <div className="task">
+
+            <span>{tarea}</span>
+
+            <button onClick={borrarTarea}>Borrar Tarea</button>
+        </div>
+    )
+}
